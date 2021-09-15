d5ee480c8a70ba34bf7d1bb0f2d660be83f97934
pjvalverde <43348020+pjvalverde@users.noreply.github.com>
2021-09-14 22:42:42 -0500

    Update mapanimation.js

--- mapanimation.js
+++ mapanimation.js
@@ -1,4 +1,4 @@
-// This array contains the coordinates for all bus stops between MIT and Harvard
+// This array contains the coordinates for all bus stops between Carondelet and National Assembly of Ecuador located in the center of Quito
 const busStops = [
       [-78.512511, -0.219641],
       [-78.512886, -0.219008],
@@ -16,21 +16,19 @@ mapboxgl.accessToken = 'pk.eyJ1IjoicGFibG92amEiLCJhIjoiY2t0aHZ5eXRoMGNybjJyanc5Y
 let map = new mapboxgl.Map({
   container: 'map',
   style:'mapbox://styles/mapbox/dark-v10',
-  center: [-78.512511, -0.219641],
+  center: [-78.512511, -0.219641], //Carondelet coordinates
   zoom: 14,
 });
 
-// TODO: add a marker to the map at the first coordinates in the array busStops. The marker variable should be named "marker"
+
 var marker = new mapboxgl.Marker()
-      .setLngLat([-78.512511, -0.219641])
+      .setLngLat([-78.512511, -0.219641]) // bus starts here in Carondelet
       .addTo(map);
 
 // counter here represents the index of the current bus stop
 let counter = 0;
 function move() {
-  // TODO: move the marker on the map every 1000ms. Use the function marker.setLngLat() to update the marker coordinates
-  // Use counter to access bus stops in the array busStops
-  // Make sure you call move() after you increment the counter.
+ 
   setTimeout(()=> {
         if(counter >= busStops.length) return;
         marker.setLngLat(busStops[counter]);
@@ -40,7 +38,7 @@ function move() {
 
 }
 
-// Do not edit code past this point
+
 if (typeof module !== 'undefined') {
   module.exports = { move };
 }
